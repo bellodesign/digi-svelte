c01238698a2f6c480cd15790369c052e291b2ca1
Marko Maberg <marko.maberg@knowit.se>
2026-03-04 23:52:15 +0100

    Tailwind tester

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,16 +35,50 @@
 
 	<digi-layout-block af-vertical-padding={true}>
 		<div class="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-8">
-			<div class="flex items-center gap-4">
-				<digi-button>Primär</digi-button>
-				<digi-button af-variation="secondary">Sekundär</digi-button>
-				<digi-button af-variation="tertiary">Tertiär</digi-button>
+			<div class="flex flex-col">
+				<h2>Knappar</h2>
+				<div class="flex flex-wrap gap-4">
+					<digi-button>Primär</digi-button>
+					<digi-button af-variation="secondary">Sekundär</digi-button>
+					<digi-button af-variation="tertiary">Tertiär</digi-button>
+				</div>
 			</div>
-			<div class="flex items-center gap-4">
-				<div class="bg-background-primary border border-border-accent-primary w-12 h-12 rounded-smaller"></div>
-				<div class="bg-background-secondary border border-border-accent-primary w-12 h-12 rounded-smaller"></div>
+			<div class="flex flex-col">
+				<h2>Tailwind-exempel</h2>
+				<p>Färger - bakgrund, ramlinje. Radius. Intellisense inkluderar digi.</p>
+				<div class="flex gap-4">
+					<div class="bg-background-primary border border-border-accent-primary w-12 h-12 rounded-smaller"></div>
+					<div class="bg-background-secondary border border-border-accent-primary w-12 h-12 rounded-smaller"></div>
+				</div>
 			</div>
 		</div>
 	</digi-layout-block>
 
+	<digi-layout-block af-vertical-padding={true}>
+		<h2>Tailwind @apply + digi spacings</h2>
+		<p>@apply inuti style-block i svelte kräver att man använder @reference för att undvika duplicering av klasser.</p>
+		<p>Exempel på spacing</p>
+		<div class="spacing-row">
+			<div class="spacing-bar pr-largest"></div>
+			<div class="spacing-bar pr-larger"></div>
+			<div class="spacing-bar pr-large"></div>
+			<div class="spacing-bar pr-base"></div>
+			<div class="spacing-bar pr-small"></div>
+			<div class="spacing-bar pr-smaller"></div>
+			<div class="spacing-bar pr-smallest"></div>
+		</div>
+	</digi-layout-block>
+
 </section>
+
+<style lang="postcss">
+	@reference "./layout.css";
+
+	.spacing-row {
+		@apply flex items-center gap-small;
+	}
+
+	.spacing-bar {
+		@apply h-base bg-background-interactive-default-primary rounded-smaller;
+	}
+</style>
