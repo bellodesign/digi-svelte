c39ecbc373dbabd1e2ea85fc0488ca15d59091b7
Marko Maberg <marko.keranen-maberg@arbetsformedlingen.se>
2026-03-10 16:04:04 +0100

    Gömmer theme-toggler så länge

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
-	import { ModeWatcher, toggleMode } from 'mode-watcher';
+	// import { ModeWatcher, toggleMode } from 'mode-watcher';
 	import './layout.css';
 
 	import { defineCustomElements } from '@designsystem-se/af/loader';
@@ -10,11 +10,11 @@
 	let { children } = $props();
 </script>
 
-<ModeWatcher />
+<!-- <ModeWatcher />
 
 <p>Lite text o grejer</p>
 
-<digi-button onafOnClick={toggleMode}>Toggla tema</digi-button>
+<digi-button onafOnClick={toggleMode}>Toggla tema</digi-button> -->
 
 <digi-typography>
 	<div class="grid min-h-dvh grid-rows-[auto_1fr_auto]">
