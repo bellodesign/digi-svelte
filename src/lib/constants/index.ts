5c0ff1162f8ffb25031677f471c7c6f3cbb246d4
Nikita Buyondo <nikita.buyondo@arbetsformedlingen.se>
2026-06-03 10:41:30 +0200

    Test

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -2,9 +2,9 @@ export const SITE_NAME = 'Digi Svelte POC';
 
 export const NAV_ITEMS = [
 	{ href: '/', label: 'Start' },
-	/* { href: '/components', label: 'Komponenter' },
+	/* { href: '/components', label: 'Komponenter' }, */
 	{ href: '/form', label: 'Formulär' },
-	{ href: '/tailwind', label: 'Digi Tailwind' },
+	/* { href: '/tailwind', label: 'Digi Tailwind' },
 	{ href: '/tailwind-demo', label: 'Tailwind Demo' },
 	{ href: '/digi-mappings', label: 'Digi Mappings' },
 	{ href: '/sok', label: 'Sök' } */
