a0a23b71ca958a59b152daf3ad4906c60eb455eb
Marko Maberg <marko.keranen-maberg@arbetsformedlingen.se>
2026-06-03 11:10:39 +0200

    Ta bort oanvänd kod

--- src/routes/forum/+page.svelte
+++ src/routes/forum/+page.svelte
@@ -12,7 +12,6 @@
 	<h1>Forum</h1>
 
 	<div class="flex flex-col gap-4">
-		<p>Lista</p>
 		{#each posts.getAll as post (post.id)}
 			<div data-id={post.id} class="bg-gray-200">
 				<h2 class="text-lg font-semibold">{post.title}</h2>
